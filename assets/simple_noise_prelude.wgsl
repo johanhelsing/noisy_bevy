bd91624f5e1848382dafe6232b3f3bc3de725ffd
Johan Klokkhammer Helsing <johanhelsing@gmail.com>
2022-10-24 22:43:29 +0200

    Add seeded versions of 2d simplex noise

--- assets/simple_noise_prelude.wgsl
+++ assets/simple_noise_prelude.wgsl
@@ -46,6 +46,47 @@ fn simplex_noise_2d(v: vec2<f32>) -> f32 {
     return 130. * dot(m, g);
 }
 
+fn simplex_noise_2d_seeded(v: vec2<f32>, seed: f32) -> f32 {
+    let C = vec4(
+        0.211324865405187, // (3.0 - sqrt(3.0)) / 6.0
+        0.366025403784439, // 0.5 * (sqrt(3.0) - 1.0)
+        -0.577350269189626, // -1.0 + 2.0 * C.x
+        0.024390243902439 // 1.0 / 41.0
+    );
+
+    // first corner
+    var i = floor(v + dot(v, C.yy));
+    let x0 = v - i + dot(i, C.xx);
+
+    // other corners
+    var i1 = select(vec2(0., 1.), vec2(1., 0.), x0.x > x0.y);
+    var x12 = x0.xyxy + C.xxzz - vec4(i1, 0., 0.);
+
+    // permutations
+    i = i % vec2(289.);
+
+    var p = permute_3(permute_3(i.y + vec3(0., i1.y, 1.)) + i.x + vec3(0., i1.x, 1.));
+    p = permute_3(p + vec3(seed));
+    var m = max(0.5 - vec3(dot(x0, x0), dot(x12.xy, x12.xy), dot(x12.zw, x12.zw)), vec3(0.));
+    m *= m;
+    m *= m;
+
+    // gradients: 41 points uniformly over a line, mapped onto a diamond
+    // the ring size, 17*17 = 289, is close to a multiple of 41 (41*7 = 287)
+    let x = 2. * fract(p * C.www) - 1.;
+    let h = abs(x) - 0.5;
+    let ox = floor(x + 0.5);
+    let a0 = x - ox;
+
+    // normalize gradients implicitly by scaling m
+    // approximation of: m *= inversesqrt(a0 * a0 + h * h);
+    m = m * (1.79284291400159 - 0.85373472095314 * (a0 * a0 + h * h));
+
+    // compute final noise value at P
+    let g = vec3(a0.x * x0.x + h.x * x0.y, a0.yz * x12.xz + h.yz * x12.yw);
+    return 130. * dot(m, g);
+}
+
 fn permute_4(x: vec4<f32>) -> vec4<f32> {
     return ((x * 34. + 1.) * x) % vec4<f32>(289.);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -34,7 +34,7 @@ fn permute_3(x: Vec3) -> Vec3 {
 
 // MIT License. © Ian McEwan, Stefan Gustavson, Munrocket, Johan Helsing
 /// Simplex noise in two dimensions
-pub fn simplex_2d(v: Vec2) -> f32 {
+pub fn simplex_noise_2d(v: Vec2) -> f32 {
     const C: Vec4 = vec4(
         0.211324865405187,
         0.366025403784439,
@@ -71,6 +71,65 @@ pub fn simplex_2d(v: Vec2) -> f32 {
         a0.y * x12.x + h.y * x12.y,
         a0.z * x12.z + h.z * x12.w,
     );
+    130. * Vec3::dot(m, g)
+}
+
+// MIT License. © Ian McEwan, Stefan Gustavson, Munrocket, Johan Helsing
+/// Simplex noise in two dimensions
+pub fn simplex_noise_2d_seeded(v: Vec2, seed: f32) -> f32 {
+    const C: Vec4 = vec4(
+        0.211324865405187,  // (3.0 - sqrt(3.0)) / 6.0
+        0.366025403784439,  // 0.5 * (sqrt(3.0) - 1.0)
+        -0.577350269189626, // -1.0 + 2.0 * C.x
+        0.024390243902439,  // 1.0 / 41.0
+    );
+
+    // first corner
+    let mut i: Vec2 = (v + Vec2::dot(v, C.yy())).floor();
+    let x0 = v - i + Vec2::dot(i, C.xx());
+
+    // other corners
+    let i1: Vec2 = if x0.x > x0.y {
+        vec2(1., 0.)
+    } else {
+        vec2(0., 1.)
+    };
+    let x12: Vec4 = x0.xyxy() + C.xxzz() - vec4(i1.x, i1.y, 0., 0.);
+
+    // permutations
+    i %= Vec2::splat(289.);
+
+    let mut p = permute_3(permute_3(i.y + vec3(0., i1.y, 1.)) + i.x + vec3(0., i1.x, 1.));
+    p = permute_3(p + Vec3::splat(seed));
+
+    let mut m = Vec3::max(
+        0.5 - vec3(
+            Vec2::dot(x0, x0),
+            Vec2::dot(x12.xy(), x12.xy()),
+            Vec2::dot(x12.zw(), x12.zw()),
+        ),
+        Vec3::splat(0.),
+    );
+    m *= m;
+    m *= m;
+
+    // gradients: 41 points uniformly over a line, mapped onto a diamond
+    // the ring size, 17*17 = 289, is close to a multiple of 41 (41*7 = 287)
+    let x = 2. * (p * C.www()).fract() - 1.;
+    let h = x.abs() - 0.5;
+    let ox = (x + 0.5).floor();
+    let a0 = x - ox;
+
+    // normalize gradients implicitly by scaling m
+    // approximation of: m *= inversesqrt(a0 * a0 + h * h);
+    m *= 1.79284291400159 - 0.85373472095314 * (a0 * a0 + h * h);
+    let g = vec3(
+        a0.x * x0.x + h.x * x0.y,
+        a0.y * x12.x + h.y * x12.y,
+        a0.z * x12.z + h.z * x12.w,
+    );
+
+    // compute final noise value at P
     130. * Vec3::dot(m, g)
 }
 
